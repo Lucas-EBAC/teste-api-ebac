629f40df53350f1073132f4eb4e96ce7b8358d4f
Lucas <lucastsunoda3@gmail.com>
2024-10-22 22:04:52 -0300

    Exercicio

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,29 +1,65 @@
 /// <reference types="cypress" />
+import contrato from '../contracts/produtos.contract'
 
 describe('Testes da Funcionalidade Usuários', () => {
 
-  it('Deve validar contrato de usuários', () => {
-    //TODO: 
+  it.only('Deve validar contrato de usuários', () => {
+    cy.request('produtos').then(response =>{
+      return contrato.validateAsync(response.body)
+    })
   });
 
   it('Deve listar usuários cadastrados', () => {
-    //TODO: 
+    cy.request({
+      method: 'GET',
+      url: 'usuarios'
+    })
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
+    cy.request({
+      method: 'POST',
+      url: 'usuarios',
+      body: {
+        "nome": "João Silva",
+        "email": "joaosilva@qa.com.br",
+        "password": "teste",
+        "administrador": "true"
+      }
+    })
   });
 
   it('Deve validar um usuário com email inválido', () => {
-    //TODO: 
+    cy.request({
+      method: 'POST',
+      url: 'usuarios',
+      body: {
+        "nome": "Matheus",
+        "email": "",
+        "password": "teste",
+        "administrador": "true"
+      }, failOnStatusCode: false
+    })
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    //TODO: 
+    cy.request({
+      method: 'PUT',
+      url: 'usuarios' + '/17ZqI1LtolLK9ThN',
+      body: {
+        "nome": "Matheus Silva",
+        "email": "jo102384@qa.com.br",
+        "password": "teste",
+        "administrador": "true",
+    },
+    })
   });
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    //TODO: 
+    cy.request({
+      method: 'DELETE',
+      url: 'usuarios' + '/7VKNDb59apmLRxiQ'
+    })
   });
 
 
